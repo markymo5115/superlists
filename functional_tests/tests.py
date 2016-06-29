5cbea59da79c18889b9eb7ca32fb5afe05ed00f5
Mark Morris <markmorris5115@hotmail.co.uk>
2016-06-29 10:27:49 +0100

    new URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -81,8 +81,8 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox.send_keys(Keys.ENTER)
 
 		# Francis gets his own unique URL
-		francis_list_url = self.browser.current.url
-		self.assertRegex(francis_list_url, '/list/.*')
+		francis_list_url = self.browser.current_url
+		self.assertRegex(francis_list_url, '/lists/.*')
 		self.assertNotEqual(francis_list_url, edith_list_url)
 
 		# Again there is no trace of Edith's list
--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,8 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+#MDM having a go, may need to delete below line
+#from django.shortcuts import render
 
 from lists.views import home_page
 from lists.models import Item
@@ -40,22 +42,13 @@ class HomePageTest(TestCase):
 		response = home_page(request)
 
 		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'],'/')
+		self.assertEqual(response['location'],'/lists/the-only-list-in-the-world/')
 
 	def test_home_page_only_saves_items_when_necessary(self):
 		request = HttpRequest()
 		home_page(request)
 		self.assertEqual(Item.objects.count(),0)
 
-	def test_home_page_displays_all_list_items(self):
-		Item.objects.create(text='itemey 1')
-		Item.objects.create(text='itemey 2')
-
-		request = HttpRequest()
-		response = home_page(request)
-
-		self.assertIn('itemey 1', response.content.decode())
-		self.assertIn('itemey 2', response.content.decode())
 
 class ItemModelTest(TestCase):
 	def test_saving_and_retrieving_items(self):
@@ -74,3 +67,30 @@ class ItemModelTest(TestCase):
 		second_saved_item = saved_items[1]
 		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
 		self.assertEqual(second_saved_item.text, 'Item the second')
+
+class ListViewTest(TestCase):
+
+	#def test_uses_list_template_myversion(self):
+	#	Item.objects.create(text='itemey 1')
+	#	Item.objects.create(text='itemey 2')
+
+	#	response = self.client.get('/lists/the-only-list-in-the-world/')
+
+	#	request = HttpRequest()
+	#	items = Item.objects.all()
+	#	renderCompare = render(request, 'list.html', {'items',items})
+	#	
+	#	self.assertEqual(renderCompare.content.decode(),response)
+
+	def test_uses_list_template(self):
+		response = self.client.get('/lists/the-only-list-in-the-world/')
+		self.assertTemplateUsed(response, 'list.html')
+
+	def test_display_all_items(self):
+		Item.objects.create(text='itemey 1')
+		Item.objects.create(text='itemey 2')
+
+		response = self.client.get('/lists/the-only-list-in-the-world/')
+
+		self.assertContains(response, 'itemey 1')
+		self.assertContains(response, 'itemey 2')
